a0a75c84569e6654d148c7a52537cc11d8f8163a
Ayush Gupta <70428309+AyushGupta-Code@users.noreply.github.com>
2024-08-28 02:44:23 +0000

    Ramanujan Code

--- HW1/myfile.py
+++ HW1/myfile.py
@@ -1,1 +1,19 @@
-### First Comment
+### Code to find one Ramanujan numbers which are a3 + b3 = c3 + d3   
+
+flag = False
+
+for i in range(1,1000):
+    if (flag):
+        break
+    for j in range (1, 1000):
+        if (flag):
+            break
+        for k in range (1, 1000):
+            if (flag):
+                break
+            for m in range(1,1000):
+                if (flag):
+                    break
+                if i!= j and j!=k and k!=m and m!=i and i!=k and i*i*i + j*j*j == k*k*k + m*m*m:
+                    print (i, j, k, m)
+                    flag = True
