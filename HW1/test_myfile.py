8cd81f5ea032be58c0292b07dbc54856f4151839
Ayush Gupta <70428309+AyushGupta-Code@users.noreply.github.com>
2024-08-29 01:30:13 +0000

    Merging 2 seperate test files into one

--- HW1/test_myfile.py
+++ HW1/test_myfile.py
@@ -2,8 +2,12 @@ import pytest
 
 from myfile import find_number
 
+# Test that will fail
 def test_find_number():
     number = find_number()
     assert number == (1, 12, 9, 1)
 
-    
+# Test that will pass
+def test_find_number():
+    number = find_number()
+    assert number == (1, 12, 9, 10)
